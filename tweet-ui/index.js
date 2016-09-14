0e86436962c9e9d87986c9346cf0bd196c9b7229
Chris Taylor <cntaylor@ca.ibm.com>
2016-09-14 16:30:15 -0400

    ct: Make the UI render...something.

--- tweet-ui/index.js
+++ tweet-ui/index.js
@@ -2,19 +2,45 @@
 
 var express = require("express"),
     Promise = require("bluebird"),
-    request = require("request"),
+    request = Promise.promisifyAll(require("request")),
+    hoffman = require("hoffman"),
+    path = require("path"),
+    util = require("util"),
     nconf = require("nconf");
 
 nconf.env().file("config/test.json");
 
 express()
-.get("/tweet.txt", (req, res) => {
-    Promise.promisify(request.get)({
+.set("views", path.join(__dirname, "public", "templates"))
+.set("view engine", "dust")
+.engine("dust", hoffman.__express())
+.get("/tweet.html", (req, res) => {
+    request.getAsync({
         url: nconf.get("TWEET_API_URL"),
         json: true
     })
     .then((r) => {
-        res.send(r.body.text);
+        var tweet = r.body;
+
+        var tweet_url = util.format(
+            "https://twitter.com/%s/status/%s",
+            tweet.user.screen_name,
+            tweet.id_str
+        );
+
+        return request.getAsync({
+            url: "https://publish.twitter.com/oembed",
+            qs: {
+                url: tweet_url,
+                omit_script: true
+            },
+            json: true
+        });
+    })
+    .then((r) => {
+        var tweetHtml = r.body.html;
+
+        res.render("index", { html: tweetHtml });
     })
     .catch((e) => {
         res.status(500).json({ error: e.toString() });
